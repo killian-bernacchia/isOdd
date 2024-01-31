ff2aee538ff7c604ec030f8695eadb267514805d
killian-bernacchia <etude.killbern@gmail.com>
2024-01-31 12:29:59 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # isOdd
-if statements suck, lookup tables are built different
+if statements suck, lookup tables are built different.
+
+To check if a number 'i' is odd you check if the bit number i is a 1 or a 0
+  `tab[i/32] & 1<<(i%32)`
+But +-*/% are not funny, so let's use only bits operators.
+  `1 & ( tab[i>>5]>>(i&0x1F) )`
+Now we got more readability and efficienty 👍
--- isOdd.c
+++ isOdd.c
@@ -3,13 +3,14 @@
 
 #include "tab.h"
 
+int isOdd(uint32_t i) { return 1 & ( tab[i>>5]>>(i&0x1F) ); }
+
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("Usage: %s <number>\n", argv[0]);
         return 1;
     }
     unsigned long num = strtoul(argv[1], NULL, 10);
-    char isOdd = 1 & ( tab[num>>5]>>(num - ((num>>5)<<5)) );
-    printf("%u",isOdd);
+    printf("%u",isOdd(num));
     return 0;
 }
