058f9468023ffa94f17f76dd5713f8247cad20f9
Lommoez <etude.killbern@gmail.com>
2025-01-10 01:19:04 +0100

    Update and rename tab.h to isOdd.h

---
+++ isOdd.h
@@ -0,0 +1,10 @@
+#ifndef ODD_ARRAY_H
+#define ODD_ARRAY_H
+
+uint32_t odd_array[134217728] = {
+	#include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h", #include "array_part.h"
+};
+
+#define isOdd(N) (1 & ( tab[i>>5]>>(i&0x1F) ))
+
+#endif
